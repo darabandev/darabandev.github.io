7b1d000567b1b91800cd73119820f9939407c273
rob <darabandev@gmail.com>
2021-03-25 17:47:12 -0400

    Finished form styling, set up form.js

---
+++ assets/js/form.js
@@ -0,0 +1,9 @@
+const nameInput = document.getElementById("name-input");
+const emailInput = document.getElementById("email-input");
+const phoneInput = document.getElementById("phone-input");
+const messageInput = document.getElementById("message-input");
+const formSubmit = document.getElementById("form-submit");
+
+formSubmit.addEventListener("click", e => {
+  console.log("hi");
+});
