f79830090a26f1942debe5113a80da701972c83c
rob <darabandev@gmail.com>
2021-03-25 18:44:46 -0400

    Form submitting correctly

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,7 +4,20 @@ const phoneInput = document.getElementById("phone-input");
 const messageInput = document.getElementById("message-input");
 const formSubmit = document.getElementById("form-submit");
 
-formSubmit.addEventListener("click", e => {
-  console.log("hi");
-  console.log("hi");
+formSubmit.addEventListener("click", async e => {
+  let data = {
+    date: new Date().toLocaleString(),
+    name: nameInput.value,
+    email: emailInput.value,
+    phone: phoneInput.value,
+    message: messageInput.value,
+  };
+
+  await fetch("https://sheet.best/api/sheets/7ac5176a-6691-4067-9188-6a14e4ae4059", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 });
